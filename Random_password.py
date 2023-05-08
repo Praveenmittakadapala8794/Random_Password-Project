bcf9cc11af42cd793e8c40c80e89e832e3b6bf6e
Praveenmittakadapala8794 <mittakadapala.praveen@gmail.com>
2023-05-08 13:28:35 +0530

    Generating a Random Password Project

---
+++ Random_password.py
@@ -0,0 +1,19 @@
+#Generating a Random Password
+
+import random
+import string
+print("Hello..welcome to password generator")
+length=int(input('\nenter the length of password:'))
+
+lower=string.ascii_lowercase
+upper=string.ascii_uppercase
+print(lower)
+print(upper)
+
+num=string.digits
+symbols=string.punctuation
+
+all=lower+upper+num+symbols
+temp=random.sample(all,length)
+password="".join(temp)
+print(password)
